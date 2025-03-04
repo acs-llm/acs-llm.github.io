95f3a4577c3bb952ce298cfc03cab726b6bda594
dev <1104463018@qq.com>
2025-03-04 10:36:28 +0800

    框架初始化，home页开发

--- components.d.ts
+++ components.d.ts
@@ -9,11 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElAffix: typeof import('element-plus/es')['ElAffix']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
-    ElHeader: typeof import('element-plus/es')['ElHeader']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
-    ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
--- src/views/KeynoteSpeakersView.vue
+++ src/views/KeynoteSpeakersView.vue
@@ -1,24 +1,11 @@
 <template>
-  <div class="keynote-speakers">
-    speakers
-  </div>
+  <div class="keynote-speakers">speakers</div>
 </template>
 
-<script>
+<script setup lang="ts">
 
 </script>
 
 <style lang="less" scoped>
-// 响应式布局：在小屏幕上调整布局
-@media (max-width: 768px) {
-  .keynote-speakers {
-    .speaker-content {
-      flex-direction: column;
-    }
 
-    .speaker-photo {
-      margin-bottom: 1rem;
-    }
-  }
-}
 </style>
